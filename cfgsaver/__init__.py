94ed824227162c3d197556d02ffbefe6c21a3e41
Prahlad Yeri <prahladyeri@yahoo.com>
2019-06-27 16:04:57 +0530

    chore: released 1.0.4

--- cfgsaver/__init__.py
+++ cfgsaver/__init__.py
@@ -3,6 +3,6 @@ __email__ = "prahladyeri@yahoo.com"
 __copyright__ = "(c) 2019 Prahlad Yeri"
 
 __license__ = "MIT"
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __title__ = 'cfgsaver'
 __description__ = 'Module to save and fetch configuration data in a standard path and format.'
